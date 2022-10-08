1b1085f08896f0e0f0dc99643a9b24c6a5d87cc6
ZhouDanny <zdannythebest@gmail.com>
2022-10-07 23:48:36 -0700

    Removed redundant code

--- movie_runner.py
+++ movie_runner.py
@@ -4,7 +4,6 @@ from movie import Movie
 def main():
     movie = Movie('8066aa78')
     response = input("Please enter a movie name: ")
-    print(movie.search(response))
     data = movie.search(response)
     print(data)
     print(f"Title: {data['Title']}\nYear: {data['Year']}\nDirector: {data['Director']}")
