83b598903f8116a8178a1f21e8f46ea36f2f0e97
ZhouDanny <zdannythebest@gmail.com>
2022-10-07 23:42:41 -0700

    Added a more generic output for the title, year, and director the movie

--- movie_runner.py
+++ movie_runner.py
@@ -5,6 +5,9 @@ def main():
     movie = Movie('8066aa78')
     response = input("Please enter a movie name: ")
     print(movie.search(response))
+    data = movie.search(response)
+    print(data)
+    print(f"Title: {data['Title']}\nYear: {data['Year']}\nDirector: {data['Director']}")
 
 
 if __name__ == "__main__":
