67b19c7627eeb97ff43c5f2f35642b87ee02e9c6
mohammedalchalabi <mohammedalchalabi@csus.edu>
2022-10-11 20:07:16 -0700

    This is a "Hello World!" Spring Boot test.

---
+++ Java/rest.service/src/main/java/com/example/rest/service/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.rest.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello World!";
+    }
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name)
+    {
+        return new Greeting(counter.incrementAndGet(),String.format(template, name));
+    }
+
+}
