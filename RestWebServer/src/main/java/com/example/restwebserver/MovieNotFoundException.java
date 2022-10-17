7a7bba8a7c5348f0906522b090f835c790d577a1
ZhouDanny <zdannythebest@gmail.com>
2022-10-17 16:44:18 -0700

    Undo

--- RestWebServer/src/main/java/com/example/restwebserver/MovieNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.restwebserver;
-
-class MovieNotFoundException extends RuntimeException
-{
-    MovieNotFoundException(Long id)
-    {
-        super("Could not find employee " + id);
-    }
-}
