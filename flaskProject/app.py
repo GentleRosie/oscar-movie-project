55d01df8e2f2b8c128876132039a9f9f82ca4a35
mohammedalchalabi <mohammedalchalabi@csus.edu>
2022-11-06 13:01:56 -0800

    This is a working MongoDB database using a cloud server. It has some minor issues but it does work and am able to upload JSON to the database.

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -7,16 +7,30 @@ Tasks:
 5. Transition to CRUD
 6.
 """
-import pymongo
-from pymongo import MongoClient
+#import pymongo
+#from pymongo import MongoClient
+
 from flask import Flask, render_template, request, url_for, redirect
+
+from bson.json_util import dumps
+
+from bson.objectid import ObjectId
+
+from flask import jsonify
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 # pip install Flask pymongo
-# client = MongoClient('localhost', 27017, username='username', password='password')
-client = pymongo.MongoClient("mongodb+srv://root:password@cluster0.d8iz1bd.mongodb.net/?retryWrites=true&w=majority")
-db = client.flask_db
-todos = db.todos
+#client = MongoClient('localhost', 27017, username='movielover99', password='movielover4life')
+#code below might be good
+#client = pymongo.MongoClient("mongodb+srv://root:password@cluster0.d8iz1bd.mongodb.net/?retryWrites=true&w=majority")
+#db = client.flask_db
+#todos = db.todos
+
+app.config['MONGO_URI'] = "mongodb+srv://movielover99:movielover4life@movies.agbzkrd.mongodb.net/movie"
+
+mongo = PyMongo(app)
+
 
 PORT = 8080
 HOST = "127.0.0.1"
@@ -29,7 +43,22 @@ def hello_world():  # put application's code here
 
 @app.get("/api/v1/movies")
 def get_movies():
-    return None
+    return jsonify("FINISH THIS METHOD!")
+
+""" THE METHOD BELOW IS NOT READY"""
+@app.post("/api/v1/movies")
+def add_movie():
+    if request.is_json:
+        response = request.get_json()
+        if 'title' in response and 'year' in response and 'director' in response:
+            #response["id"] = _find_next_id(user_data)
+            #user_data.append(response)
+            id = mongo.db.movie.insert_one({'title':response['title'],'year':response['year'],'director':response['director']})
+            return response, 201
+        else:
+            return {"error": "Malformed request. Missing required user fields"}, 400
+    return {"error": "Request must be JSON"}, 415
+
 # @app.route('/', methods=('GET', 'POST'))
 # def index():
 #     return render_template('index.html')
--- flaskProject/db.py
+++ flaskProject/db.py
@@ -4,7 +4,10 @@ db = client.test
 DANNY do this
 pip install Flask pymongo
 """
+import pymongo
 
+client = pymongo.MongoClient("mongodb+srv://root:password@cluster0.d8iz1bd.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
 
 import sqlite3
 
@@ -28,3 +31,19 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
