33f608b0056d155d422dd2f44e97e1e0865f77f9
ZhouDanny <zdannythebest@gmail.com>
2022-12-04 12:32:31 -0800

    Adjusted user Rest API calls and added error handling

--- OscarMovieProject/app.py
+++ OscarMovieProject/app.py
@@ -1,28 +1,37 @@
-from math import floor
-from flask import Flask, render_template
+# from math import floor
+import json
+
+from flask import Flask  # , render_template
 from flask import request
 from MovieAPI import MovieAPI
 from json import load
-from flask import jsonify
+# from flask import jsonify
 from flask_pymongo import PyMongo
-import subprocess as sp
-import string
-
-app = Flask(__name__, template_folder='client/public')
-
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/Movies'
-mongo_client = PyMongo(app)
-mongo_collection = mongo_client.db['movie']
-db = mongo_collection
-ID = 0
+from bson.objectid import ObjectId
+# import subprocess as sp
 
 
-@app.route('/', methods=('GET', 'POST'))
-def index():
-    list = db.find()
-    return render_template('index.html', list=list)
+class MyEncoder(json.JSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return super(MyEncoder, self).default(o)
 
 
+app = Flask(__name__)   # , template_folder='client/public') look into what template_folder does
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/MovieProject'
+app.json_encoder = MyEncoder
+mongo = PyMongo(app)
+academy_awards_file = open('new_data_json.json', encoding='utf-8')
+academy_awards_data = load(academy_awards_file)
+movie_api = MovieAPI('https://www.omdbapi.com/?apikey=', '8066aa78')
+
+
+# @app.route('/', methods=('GET', 'POST'))
+# def index():
+#     movie_list = mongo.db.movies.find()
+#     return render_template('index.html', list=movie_list)
 
 
 # @app.get("/api/v2/movies")
@@ -31,55 +40,50 @@ def index():
 #     for movie in db.find():
 #         result.append({'title' : movie['title'], 'year' : movie['year'], 'director' : movie['director']})
 #     return jsonify({'result' : result})
+# method was used in conjunction with React JS
+# @app.route('/add', methods = ['POST'])
+# def add_user():
+#     _json = request.json
+#     _name = _json['name']
+#     _email = _json['email']
+#     _password = _json['password']
+#
+#     if _name and _email and _password and request.method == 'POST':
+#         mongo.db.movies.insert_one({'name': _name, 'email': _email, 'password': _password})
+#         response = jsonify('User added successfully!')
+#         response.status_code = 200
+#         return response
+#     else:
+#         return not_found()
+# @app.errorhandler(404)
+# def not_found(error=None):
+#     message = {
+#         'status': 404,
+#         'message': 'Not Found' + request.url
+#     }
+#     response = jsonify(message)
+#
+#
+# @app.route('/names', methods = ['GET'])
+# def default():
+#     return {"names": ["Alan", "Edgar", "Poe"]}
 
 
-@app.route('/add', methods = ['POST'])
-def add_user():
-    _json = request.json
-    _name = _json['name']
-    _email = _json['email']
-    _password = _json['password']
-
-    if _name and _email and _password and request.method == 'POST':
-        db.movies.insert_one({'name': _name, 'email': _email, 'password': _password})
-        response = jsonify('User added successfully!')
-        response.status_code = 200
-        return response
-    else:
-        return not_found()
+@app.get("/api/v1/omdb/movies")
+def get_movie_data():
+    movie_title = request.args['title']
+    movie_data = movie_api.search_movie_title(movie_title)
+    if len(movie_data) == 2:
+        return {"error": f"No data found for movie {movie_title}"}, 404
+    return {'director': movie_data['Director'], 'language': movie_data['Language'],
+            'title': movie_data['Title'], 'year': movie_data['Year']}
 
 
-@app.errorhandler(404)
-def not_found(error=None):
-    message = {
-        'status': 404,
-        'message': 'Not Found' + request.url
-    }
-    response = jsonify(message)
-
-
-@app.route('/names', methods = ['GET'])
-def default():
-    return {"names": ["Alan", "Edgar", "Poe"]}
-
-
-@app.get("/api/v1/movies")
-def get_movie_title():
-    film = request.args['title']  # use ?title= to make request
-    api = MovieAPI('https://www.omdbapi.com/?apikey=', '8066aa78')
-    data = api.search_movie_title(film)
-    if len(data) == 2:
-        return {"error": f"No data found for movie {film}"}, 404
-    return {"director": data["Director"], "langauge": data["Language"], "title": data["Title"], "year": data["Year"]}
-
-
-@app.get("/api/v1/movies/oscars/<int:year>")
+@app.get("/api/v1/oscars/movies/<int:year>")
 def get_oscar_nominees_by_year(year: int):
     if year < 1927 or year > 2019:
         return {"error": f"No data found for year {year}"}, 404
 
-    academy_awards_file = open('new_data_json.json', encoding='utf-8')
-    academy_awards_data = load(academy_awards_file)
     winners_list = []
     nominees_list = []
 
@@ -90,11 +94,10 @@ def get_oscar_nominees_by_year(year: int):
             else:
                 nominees_list.append(award)
 
-    academy_awards_file.close()
     return {"winners": winners_list, "nominees": nominees_list}
 
 
-def _helper(academy_awards_data: list, year: int, categories: list) -> list:
+def _helper(category: str, year: int, categories: list) -> dict:
     winners_list = []
     for award in academy_awards_data:
         if award['year'] == year and award['winner']:
@@ -102,106 +105,110 @@ def _helper(academy_awards_data: list, year: int, categories: list) -> list:
                 if award['category'] == category:
                     winners_list.append(award)
                     break
-    return winners_list
+    winner_movie_title = winners_list[0]['film']
+    winner_movie_year = winners_list[0]['year']
+    print(f'{winner_movie_title}, {winner_movie_year}')
+    movie_data = movie_api.search_movie_title_and_year(winner_movie_title, winner_movie_year)
+    mongo.db.movies.insert_one({f'{category} of year {year}': movie_data})
+    return {f'{category} of year {year}': movie_data}
 
 
-@app.get("/api/v1/movies/oscars/best_picture/<int:year>")
+@app.get("/api/v1/oscars/movies/best_picture/<int:year>")
 def get_oscar_best_picture_winners_by_year(year: int):
     if year < 1927 or year > 2019:
         return {"error": f"No data found for year {year}"}, 404
 
-    academy_awards_file = open('new_data_json.json', encoding='utf-8')
-    academy_awards_data = load(academy_awards_file)
-    winners_list = _helper(academy_awards_data, year, ['OUTSTANDING PICTURE','OUTSTANDING PRODUCTION',
-                                                       'OUTSTANDING MOTION PICTURE','BEST MOTION PICTURE',
-                                                       'BEST PICTURE'])
-    academy_awards_file.close()
-    # global ID
-    # api = MovieAPI('https://www.omdbapi.com/?apikey=', '8066aa78')
-    # movie = winners_list[0]['film']
-    # year = winners_list[0]['year']
-    # data = api.search_movie_title_and_year(movie, year)
-    # print(data)
-    # db.movies.insert_one({'id': ID, 'data': data})
-    # ID = ID + 1
-    return winners_list
+    return _helper('Best Picture', year, ['OUTSTANDING PICTURE', 'OUTSTANDING PRODUCTION',
+                                          'OUTSTANDING MOTION PICTURE', 'BEST MOTION PICTURE', 'BEST PICTURE'])
 
 
-@app.get("/api/v1/movies/oscars/best_actor/<int:year>")
+@app.get("/api/v1/oscars/movies/best_actor/<int:year>")
 def get_oscar_best_actor_winners_by_year(year: int):
     if year < 1927 or year > 2019:
         return {"error": f"No data found for year {year}"}, 404
 
-    academy_awards_file = open('new_data_json.json', encoding='utf-8')
-    academy_awards_data = load(academy_awards_file)
-    winners_list = _helper(academy_awards_data, year, ['ACTOR','ACTOR IN A LEADING ROLE',
-                                                       'ACTRESS IN A LEADING ROLE','ACTRESS'])
-    academy_awards_file.close()
-    return winners_list
+    return _helper('Best Actor', year, ['ACTOR', 'ACTOR IN A LEADING ROLE', 'ACTRESS IN A LEADING ROLE', 'ACTRESS'])
 
 
-@app.get("/api/v1/movies/recommendation")
-def get_movie_recommendation():
-    year_frequency = [0] * 10
-    index = None
-    test_data = [1969, 2019, 1981, 1974, 2016, 1989, 1946]
-    for year in test_data:
-        if test_data < 2000:
-            index = floor((year % 100) / 10) * 10
-        else:
-            index = 9 + floor((year % 100) / 10) * 10
-    # query database based on highest years searched then return movie
+# @app.get("/api/v1/oscars/movies/recommendation") WORKING ON IMPLEMENTATION
+# def get_movie_recommendation():
+#     year_frequency = [0] * 10
+#     index = None
+#     test_data = [1969, 2019, 1981, 1974, 2016, 1989, 1946]
+#     for year in test_data:
+#         if test_data < 2000:
+#             index = floor((year % 100) / 10) * 10
+#         else:
+#             index = 9 + floor((year % 100) / 10) * 10
+#     # query database based on highest years searched then return movie
 
 
-@app.get("/api/v2/movies")
+@app.get("/api/v1/user/movies")
 def get_all_movies():
-    result = []
-    for movie in db.find():
-        result.append({'title' : movie['title'], 'year' : movie['year'], 'director' : movie['director']})
-    return jsonify({'result' : result})
+    response = []
+    for movie_data in mongo.db.movies.find():
+        response.append({'director': movie_data['director'], 'language': movie_data['language'],
+                         'title': movie_data['title'], 'year': movie_data['year']})
+    return {'Movies': response}
 
 
-@app.get("/api/v2/movies/<string:movie_title>")
-def get_one_movie(movie_title: string):
-    movie = db.find_one({'title' : movie_title})
-    result = []
-    result.append({'title' : movie['title'], 'year' : movie['year'], 'director' : movie['director']})
-    return jsonify({'result' : result})
+@app.get("/api/v1/user/movies/<string:movie_title>")
+def get_one_movie(movie_title: str):
+    movie_data = mongo.db.movies.find_one({'title': movie_title})
+    response = {'director': movie_data['director'], 'language': movie_data['language'],
+                'title': movie_data['title'], 'year': movie_data['year']}
+    return {'Movie': response}
 
 
-@app.post("/api/v2/movies/")
-def add_movie():
-    if request.is_json:
-        response = request.get_json()
-        if 'title' in response and 'year' in response and 'director' in response:
-            #response["id"] = _find_next_id(user_data)
-            #user_data.append(response)
-            id = db.insert_one({'title':response['title'],'year':response['year'],'director':response['director']})
-            return response, 201
-        else:
-            return {"error": "Malformed request. Missing required user fields"}, 400
-    return {"error": "Request must be JSON"}, 415
+@app.post("/api/v1/user/movies")
+def add_one_movie():
+    if not request.is_json:
+        return {'Error': 'Request must be JSON'}, 415
+
+    if not ('director' in request.json and 'language' in request.json and
+            'title' in request.json and 'year' in request.json):
+        return {'Error': 'Malformed request. Missing required movie fields'}, 400
+
+    response = {'director': request.json.get('director'), 'language': request.json.get('language'),
+                'title': request.json.get('title'), 'year': request.json.get('year')}
+    mongo.db.movies.insert_one(response)
+    return response, 201
 
 
-@app.put("/api/v2/movies/<string:movie_title>")
-def edit_movie_data(movie_title: string):
-    if request.is_json:
-        if 'title' in request.json or 'year' in request.json or 'director' in request.json:
-            myquery = {'title' : movie_title}
-            newvalues = { "$set": { "title": request.json.get('title'), "year": request.json.get('year'), "director": request.json.get('director') } }
-            db.update_one(myquery, newvalues)
-            return jsonify("done!"), 200
-        else:
-            return {"error": "Malformed request. Missing required user fields"}, 400
-    return {"error": "Request must be JSON"}, 415
+@app.put("/api/v1/user/movies/<string:movie_title>")
+def edit_one_movie(movie_title: str):
+    movie_format = {'title': movie_title}
+    movie_data = mongo.db.movies.find_one(movie_format)
+
+    if movie_data is None:
+        return {'Error': f'No data found for movie {movie_title}'}, 404
+
+    if not request.is_json:
+        return {'Error': 'Request must be JSON'}, 415
+
+    if not ('director' in request.json or 'language' in request.json or
+            'title' in request.json or 'year' in request.json):
+        return {'Error': 'Malformed request. Missing required movie fields'}, 400
+
+    response = {'title': request.json.get('title') or movie_data['title'],
+                'director': request.json.get('director') or movie_data['director'],
+                'language': request.json.get('language') or movie_data['language'],
+                'year': request.json.get('year') or movie_data['year']}
+    mongo.db.movies.update_one(movie_format, {'$set': response})
+    return response, 200
 
 
-@app.delete("/api/v2/movies/<string:movie_title>")
-def delete_user(movie_title: string):
-    myquery = {"title": movie_title}
-    movie = db.find_one(myquery)
-    db.delete_one(myquery)
-    return jsonify({'title' : movie['title'], 'year' : movie['year'], 'director' : movie['director']}), 200
+@app.delete("/api/v1/user/movies/<string:movie_title>")
+def delete_one_movie(movie_title: str):
+    movie_format = {"title": movie_title}
+    movie_data = mongo.db.movies.find_one(movie_format)
+
+    if movie_data is None:
+        return {'Error': f'No data found for movie {movie_title}'}, 404
+
+    mongo.db.movies.delete_one(movie_format)
+    return {'director': movie_data['director'], 'language': movie_data['language'],
+            'title': movie_data['title'], 'year': movie_data['year']}, 200
 
 
 if __name__ == '__main__':
