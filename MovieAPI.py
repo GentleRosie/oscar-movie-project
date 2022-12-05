a8cd78d31a46f504a10dca71477f6f18ad391526
Branden <brandennguyen17@gmail.com>
2022-12-05 15:43:13 -0800

    Fixed MovieAPI being static

--- MovieAPI.py
+++ MovieAPI.py
@@ -6,13 +6,14 @@ from json import loads
 class MovieAPI(RestAPI):
     def __init__(self, url: str, key: str):
         super().__init__(url, key)
+        self.api_request = None
 
     def search_movie_title(self, title: str):
         title = title.replace(' ', '+')
-        self.api = f'{self.api}t={title}'
-        return loads(urlopen(self.api).read())
+        self.api_request = f'{self.api}t={title}'
+        return loads(urlopen(self.api_request).read())
 
     def search_movie_title_and_year(self, title: str, year: int):
         title = title.replace(' ', '+')
-        self.api = f'{self.api}t={title}&y={year}'
-        return loads(urlopen(self.api).read())
+        self.api_request = f'{self.api}t={title}&y={year}'
+        return loads(urlopen(self.api_request).read())
--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.json_encoder = MyEncoder
 mongo = PyMongo(app)
 academy_awards_file = open('new_data_json.json', encoding='utf-8')
 academy_awards_data = load(academy_awards_file)
-movie_api = MovieAPI('https://www.omdbapi.com/?apikey=', '8066aa78')
+movie_api = MovieAPI('https://www.omdbapi.com/?apikey=', '902f9339')
 
 
 # @app.route('/', methods=('GET', 'POST'))
